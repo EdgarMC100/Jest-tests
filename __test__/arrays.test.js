a369f811400a85fe17863726ff110741008a44b8
EdgarMC100 <edgarmc100@gmail.com>
2021-07-21 06:02:44 -0500

    Adding test coverage

--- __test__/arrays.test.js
+++ __test__/arrays.test.js
@@ -4,12 +4,13 @@ describe('Comprobaremos que existe un elemento', () => {
     test('Tiene una banana?', ()=>{
         expect(arrayFruits()).toContain('banana')
     })
-
     test('No contiene un Platano', () => {
         expect(arrayFruits()).not.toContain('platano')
     })
-
     test('Comprobar el tamaño de un arreglo', () => {
         expect(arrayFruits()).toHaveLength(6)
+    })
+    test('Comprobaremos que existe un color', () => {
+        expect(arrayColors()).toContain('azul')
     })
 });
